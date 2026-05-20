730faeb29c4a8bf936977e820d9883dd058ba512
Martin Kuckert <martin@curlybracket.de>
2026-05-20 08:21:17 +0200

    MTPLX listenes to all IPs

--- mtplx/serve.sh
+++ mtplx/serve.sh
@@ -7,6 +7,7 @@ ctx_size=$((ctx * parallel))
 mtplx serve \
   --model /Users/mkuckert/ai/models/mtplx/Youssofal--Qwen3.6-27B-MTPLX-Optimized-Speed \
   --cache-dir /Users/mkuckert/ai/models/mtplx \
+  --host 0.0.0.0 \
   --port $MTPLX_PORT \
   --api-key $MTPLX_API_KEY \
   --no-stats-footer \
